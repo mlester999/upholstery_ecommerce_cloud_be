59205f030fd7e5eb55df02729091fcd0f182532c
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-28 16:38:00 +0800

    Update ormconfig.ts

--- src/db/ormconfig.ts
+++ src/db/ormconfig.ts
@@ -21,7 +21,7 @@ import { DataSourceOptions, DataSource } from 'typeorm';
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: +process.env.POSTGRES_PORT,
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
